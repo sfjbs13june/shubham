1f047716d0492773555f632517a545d7a6b9354f
sfjbs <shubham.kumar41@tcs.com>
2023-01-13 06:37:42 +0000

    Day 02

---
+++ Day02/second-spring-boot-app/src/main/java/com/shubham/app/SecondSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringBootAppApplication.class, args);
+	}
+
+}
